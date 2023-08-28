94fe5755434e8e338faa551757cacbed9708b816
Xiyue Zhang <xiyuej.zhang@mail.utoronto.ca>
2023-08-28 10:55:51 -0700

    Initial Commit

---
+++ CareCount/CareCountApp.swift
@@ -0,0 +1,17 @@
+//
+//  CareCountApp.swift
+//  CareCount
+//
+//  Created by Jasmine Zhang on 8/28/23.
+//
+
+import SwiftUI
+
+@main
+struct CareCountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
