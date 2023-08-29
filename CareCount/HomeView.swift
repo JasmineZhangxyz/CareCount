38b89f6e83d2f88decc60f12b16b8b2ea7b4da1e
Xiyue Zhang <xiyuej.zhang@mail.utoronto.ca>
2023-08-29 00:35:44 -0700

    adds cat pictures to home page

--- CareCount/HomeView.swift
+++ CareCount/HomeView.swift
@@ -12,6 +12,9 @@ struct HomeView: View {
     @State private var quoteText: String = ""
     @State private var author: String = ""
     
+    // for cat picture
+    @State private var imageUrl: URL?
+    
     // for navbar footer
     init() {
         UITabBar.appearance().isHidden = true
@@ -27,20 +30,44 @@ struct HomeView: View {
                     .fontWeight(.bold)
                     .foregroundColor(Color("darkPink"))
                 
+                // cat picture
+                if let imageUrl = imageUrl {
+                    AsyncImage(url: imageUrl) { image in
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 300)
+                            .cornerRadius(10)
+                            .padding()
+                    } placeholder: {
+                        Color("backgroundPink") // Placeholder while loading
+                    }
+                }
+                
+                // quote text
                 Text(quoteText)
                     .font(.body)
                     .italic()
                     .foregroundColor(.black)
-                    .padding([.top, .leading, .trailing])
+                    .frame(width: 300)
+                    .padding(.top)
+                    .padding(.bottom, 5)
                 
+                // quote author
                 Text("- \(author)")
                     .font(.body)
                     .italic()
                     .foregroundColor(.black)
                     .padding(.bottom)
             }
-            .onAppear(perform: fetchQuoteOfTheDay)
+            .onAppear(perform: fetchQuoteAndImage)
         }
+    }
+    
+    // fetch quote + image
+    func fetchQuoteAndImage() {
+        fetchQuoteOfTheDay()
+        fetchCatImage()
     }
     
     // fetch quote of the day
@@ -75,10 +102,44 @@ struct HomeView: View {
                 print("JSON parsing error: \(error)")
             }
         }
-        
+        task.resume()
+    }
+    
+    func fetchCatImage() {
+        guard let imageUrl = URL(string: "https://api.thecatapi.com/v1/images/search") else {
+            return
+        }
+            
+        let session = URLSession.shared
+            
+        let task = session.dataTask(with: imageUrl) { (data, response, error) in
+            if let error = error {
+                print("Error: \(error)")
+                return
+            }
+                
+            guard let data = data else {
+                return
+            }
+                
+            do {
+                if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [[String: Any]],
+                    let firstImage = json.first,
+                    let imageUrlString = firstImage["url"] as? String,
+                    let imageUrl = URL(string: imageUrlString) {
+                        DispatchQueue.main.async {
+                            self.imageUrl = imageUrl
+                    }
+                }
+            } catch {
+                print("JSON parsing error: \(error)")
+            }
+        }
+            
         task.resume()
     }
 }
+
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
